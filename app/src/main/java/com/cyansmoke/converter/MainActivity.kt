837ab29b4b1a45e329ef9ba1c8cc24a71c536ca0
Tim Kuzmin <38453597+timtimdotqz@users.noreply.github.com>
2019-01-27 19:33:05 +0300

    Update MainActivity.kt

--- app/src/main/java/com/cyansmoke/converter/MainActivity.kt
+++ app/src/main/java/com/cyansmoke/converter/MainActivity.kt
@@ -23,34 +23,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 // нажимается кнопка Enter на клавиатуре и валюта переводится
 
 class MainActivity : AppCompatActivity() {
-
-    val BASE_URL = "https://free.currencyconverterapi.com/api/v6/"
-    val retrofit: Retrofit =
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-    val retrofitConnector = retrofit.create(ApiService::class.java)
-    var currencies: List<String>? = null
-    var firstCurrency: String? = null
-    var secondCurrency: String? = null
+    
+    object NetworkManager {
+        
+        val BASE_URL = "https://free.currencyconverterapi.com/api/v6/"
+        val retrofit: Retrofit = Retrofit.Builder()
+                                         .baseUrl(CurrenciesApiService.BASE_URL)
+                                         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                                         .build()
+        val currenciesApi = retrofit.create(CurrenciesApiService::class.java)
+        
+    }
+    
+    private var currencies: List<String>? = null
+    private var firstCurrency: String? = null
+    private var secondCurrency: String? = null
     //map для сохранения кэша пар валют в виде "пара" : "значение"
-    val mapCurrencies: MutableMap<String, Double> = mutableMapOf()
+    private val mapCurrencies: MutableMap<String, Double> = mutableMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        setupListeners()
+    }
+    
+    private fun setupListeners() {
         //переопределяем нажатие на кнопку Done(изменённый Enter на клавиатуре)
-        editTextFirst.setOnEditorActionListener { _, actionId, _ ->
-            return@setOnEditorActionListener when (actionId) {
-                EditorInfo.IME_ACTION_DONE -> {
-                    //запрос на валютную пару
-                    giveMePair(firstCurrency, secondCurrency, editTextFirst, editTextSecond)
-                    true
-                }
-                else -> false
-            }
-        }
-        editTextSecond.setOnEditorActionListener { _, actionId, _ ->
+        val editorActionListener: (editText: EditText, actionId: Int) -> Boolean = { _, actionId, _ ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_DONE -> {
                     giveMePair(secondCurrency, firstCurrency, editTextSecond, editTextFirst)
@@ -59,7 +59,16 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-        updateSpinnerAdapter()
+        
+        editTextFirst.setOnEditorActionListener { editorActionListener.confirm() }
+        editTextSecond.setOnEditorActionListener { editorActionListener.confirm() }
+        
+        getActualCurrencies { c ->
+            currencies = c ?: currencies
+            setupSpinnerAdapter(firstSpinner, firstProgressBar)
+            setupSpinnerAdapter(secondSpinner, secondProgressBar)
+        }
+        
         //для каждого спиннера берём выбранное значение и сохраняем в переменную
         spinnerFirst?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -83,44 +92,54 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun updateSpinnerAdapter() {
+    private fun getActualCurrencies(completion: ((currencies: List<String>?) -> Unit)? = null) {
         retrofitConnector.getListOfCurrencies().enqueue(object : Callback<JsonObject> {
 
             override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
                 val results = response.body()?.getAsJsonObject("results") ?: return
                 //здесь обрабатывается полученный обьект, список валют заносится в Spinner
-                currencies = results.entrySet().map { it.key }.toList()
-                val currencies = currencies ?: return
-                val adapter = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_spinner_item, currencies)
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinnerFirst.adapter = adapter
-                spinnerSecond.adapter = adapter
-                progressBarFirst.showIf(true)
-                progressBarSecond.showIf(true)
-                spinnerFirst.showIf(false)
-                spinnerSecond.showIf(false)
-
+                val currencies = results.entrySet().map { it.key }.toList()
+                completion?.invoke(currencies)
             }
 
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "Check your internet connection and restart app", Toast.LENGTH_LONG)
+                Toast
+                    .makeText(this@MainActivity, 
+                              "Check your internet connection and restart app", 
+                              Toast.LENGTH_LONG)
                     .show()
+                completion?.invoke(null)
             }
         })
     }
+    
+    private fun setupSpinnerAdapter(spinner: Spinner, progressBar: ProgressBar) {
+        val currencies = currencies ?: return
+        
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        
+        spinner.adapter = adapter
+        progressBar.showIf(true)
+        spinner.showIf(false)
+    }
 
     private fun giveMePair(first: String?, second: String?, editTextOne: EditText, editTextTwo: EditText) {
-        if (editTextOne.text.toString() != "") {
+        if (editTextOne.text.toString().isEmpty()) {
             progressBarMain.showIf(false)
-            retrofitConnector.getCourse(first + "_" + second)
+            
+            val currencyRequest = first + "_" + second
+            
+            retrofitConnector.getCourse(currencyRequest)
                 .enqueue(object : Callback<JsonObject> {
+                    
                     override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                        progressBarMain.showIf(true)
-                        val coefficient =
-                            response.body()?.getAsJsonPrimitive(first + "_" + second).toString()
-                                .toDouble()
+                        val body = response.body() ?: return onFailure(call, Error("empty body"))
+                        val coefficient = body.getAsJsonPrimitive(currencyRequest).getAsDouble()
                         val sum = coefficient * editTextOne.text.toString().toDouble()
-                        mapCurrencies.putIfAbsent(first + "_" + second, coefficient)
+                        
+                        progressBarMain.showIf(true)
+                        mapCurrencies.put(currencyRequest, coefficient)
                         editTextTwo.setText(sum.toString())
                     }
 
@@ -128,9 +147,9 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(
                             this@MainActivity,
                             "internet connection is bad or missing, trying to check cache...",
-                            Toast.LENGTH_SHORT
-                        )
+                            Toast.LENGTH_SHORT)
                             .show()
+                        
                         //проверяем пытались ли мы раньше перевести эти валюты
                         if (mapCurrencies.contains(first + "_" + second)) {
                             Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
